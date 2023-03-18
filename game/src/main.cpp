9029b2636b0779b5d08004d3329430c1410d0949
arngo <27396817+arngo@users.noreply.github.com>
2023-03-18 19:42:10 -0400

    make use of frame time and added textures to obstacles

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -4,7 +4,11 @@
 class Player {
     public:
         int health;
+        bool isDead = false;
         Rectangle hitbox;
+        float speed = 200.0f;
+        float damageCooldown = 1.0f;
+        float damageTimer = damageCooldown;
 
         Player(Rectangle hitbox, int health) : 
             hitbox(hitbox),
@@ -14,42 +18,64 @@ class Player {
 
 class Obstacle {
     public:
+        int damage;
         Rectangle hitbox;
-        Obstacle(Rectangle hitbox) : 
-            hitbox(hitbox)
+        Texture2D texture;
+        Obstacle(Rectangle hitbox, int damage, std::string textureFile) : 
+            hitbox(hitbox),
+            damage(damage),
+            texture(LoadTexture(textureFile.c_str()))
         {}
+        ~Obstacle() {
+            UnloadTexture(texture);
+        }
+
 };
 
 int main ()
 {
 	// set up the window
 	InitWindow(1280, 800, "Spaceship game");
-    Player player(Rectangle{100.0f, 100.0f, 20.0f, 20.0f}, 10);
-
-    Obstacle o(Rectangle{300.0f, 200.0f, 30.0f, 30.0f});
 
     SetTargetFPS(60); 
+    float frameTime;
 	
+    Player player(Rectangle{100.0f, 100.0f, 20.0f, 20.0f}, 3);
+    Obstacle o(Rectangle{300.0f, 200.0f, 128.0f, 128.0f}, 3, "./resources/asteroid.png");
+
 	// game loop
 	while (!WindowShouldClose())
 	{
-        if (IsKeyDown(KEY_RIGHT)) player.hitbox.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) player.hitbox.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) player.hitbox.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) player.hitbox.y += 2.0f;
-		// drawing
-		BeginDrawing();
-		ClearBackground(BLACK);
-        std::cout << player.hitbox.x << " " << player.hitbox.y << "\n";
-        DrawRectangleRec(player.hitbox, RAYWHITE);
-        DrawRectangleRec(o.hitbox, RED);
-		//DrawText("Hello Raylib", 200,200,20,WHITE);
-        if (player.health <= 0) {
+        frameTime = GetFrameTime();
+        if (IsKeyDown(KEY_RIGHT)) {
+            player.hitbox.x += frameTime * player.speed;
+        }
+        if (IsKeyDown(KEY_LEFT)) {
+            player.hitbox.x -= frameTime * player.speed;
+        }
+        if (IsKeyDown(KEY_UP)) {
+            player.hitbox.y -= frameTime * player.speed;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            player.hitbox.y += frameTime * player.speed;
+        }
+        player.damageTimer += frameTime;
+        if (!player.isDead && player.health <= 0) {
             std::cout << "player dead\n";
-        } else if (CheckCollisionRecs(player.hitbox, o.hitbox)) {
-            player.health -= 1;
+            player.isDead = true;
+        } else if (!player.isDead && player.damageTimer >= player.damageCooldown && CheckCollisionRecs(player.hitbox, o.hitbox)) {
+            player.health -= o.damage;
+            player.damageTimer = 0.0f;
             std::cout << "health: " << player.health << "\n";
         }
+
+		// drawing
+		BeginDrawing();
+            ClearBackground(BLACK);
+            //std::cout << player.hitbox.x << " " << player.hitbox.y << "\n";
+            DrawRectangleRec(player.hitbox, RAYWHITE);
+            //DrawRectangleRec(o.hitbox, RED);
+            DrawTexturePro(o.texture, Rectangle{0,0,64,64}, o.hitbox, Vector2{0,0}, 0.0f, WHITE);
 		EndDrawing();
 	}
 
