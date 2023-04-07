2d8af0d6181a992e3874ff34322dce519100297e
arngo <27396817+arngo@users.noreply.github.com>
2023-04-06 21:08:31 -0400

    implement fuel, death screen, object despawning
    (incomplete)
    
    - create a base class for non-player objects
    - generalize object collision
    - disable player controls when dead
    - despawn objects when they go off screen
    
    todo: despawn fuel when collected

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,9 +9,11 @@ class Player {
         int health;
         bool isDead = false;
         Rectangle hitbox;
-        float speed = 200.0f;
+        float speed = 400.0f;
         float damageCooldown = 1.0f;
         float damageTimer = damageCooldown;
+        float fuel = 100.0f;
+        float fuelConsumption = 1.0f;
 
         Player(Rectangle hitbox, int health) : 
             hitbox(hitbox),
@@ -19,17 +21,51 @@ class Player {
         {}
 };
 
-class Obstacle {
+class WorldObject {
+    public:
+        Rectangle hitbox;
+        Rectangle textureSize;
+        Texture2D texture;
+        WorldObject(Rectangle hitbox, std::string textureFile, Rectangle textureSize) : 
+            hitbox(hitbox),
+            texture(LoadTexture(textureFile.c_str())),
+            textureSize(textureSize)
+
+        {}
+        virtual void action(Player& player) {
+        }
+        ~WorldObject() {
+            UnloadTexture(texture);
+        }
+};
+
+class Obstacle: public WorldObject {
     public:
         int damage;
-        Rectangle hitbox;
-        Texture2D texture;
-        Obstacle(Rectangle hitbox, int damage, std::string textureFile) : 
-            hitbox(hitbox),
-            damage(damage),
-            texture(LoadTexture(textureFile.c_str()))
+        
+        Obstacle(Rectangle hitbox, int damage, std::string textureFile, Rectangle textureSize) : WorldObject{hitbox, textureFile, textureSize},
+            damage(damage)
         {}
+        void action(Player& player) {
+            player.health -= damage;
+            player.damageTimer = 0.0f;
+        }
         ~Obstacle() {
+            UnloadTexture(texture);
+        }
+};
+
+class Fuel: public WorldObject {
+    public:
+        float fuel;
+        
+        Fuel(Rectangle hitbox, float fuel, std::string textureFile, Rectangle textureSize) : WorldObject{hitbox, textureFile, textureSize},
+            fuel(fuel)
+        {}
+        void action(Player& player) {
+            player.fuel += fuel;
+        }
+        ~Fuel() {
             UnloadTexture(texture);
         }
 };
@@ -40,10 +76,19 @@ Obstacle* spawnObstacle()
 	// Offset = 0
 	// Range = 1281
 	x = 0 + (rand() % SCREEN_WIDTH + 1);
-    return new Obstacle(Rectangle{x, 200.0f, 128.0f, 128.0f}, 3, "./resources/asteroid.png");
+    return new Obstacle(Rectangle{x, -100.0f, 128.0f, 128.0f}, 1, "./resources/asteroid.png", Rectangle{0,0,64,64});
 }
 
-Obstacle* checkObstacleCollisions(Player &player, std::vector<Obstacle*> &obstacles) {
+Fuel* spawnFuel()
+{
+    float x;
+	// Offset = 0
+	// Range = 1281
+	x = 0 + (rand() % SCREEN_WIDTH + 1);
+    return new Fuel(Rectangle{x, -100.0f, 86.0f, 86.0f}, 20, "./resources/fuel.png", Rectangle{0,0,43,43});
+}
+
+WorldObject* checkObjectCollisions(Player &player, std::vector<WorldObject*> &obstacles) {
   for (auto& o : obstacles) {
     if (CheckCollisionRecs(player.hitbox, o->hitbox)) {
       return o; 
@@ -56,11 +101,15 @@ int main ()
 {
 	// set up the window
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Spaceship game");
-    std::vector<Obstacle*> obstacles;
-    float obstacleTimer = 0;
+    std::vector<WorldObject*> objects;
+    float objectTimer = 0;
+    float flyingSpeed = 300.0f;
 
     SetTargetFPS(60); 
     float frameTime;
+
+    std::string deadText = "You died";
+    int deadTextWidth = MeasureText(deadText.c_str(), 50);
 	
     Player player(Rectangle{SCREEN_WIDTH/2, 600.0f, 20.0f, 20.0f}, 3);
     //Obstacle o(Rectangle{300.0f, 200.0f, 128.0f, 128.0f}, 3, "./resources/asteroid.png");
@@ -69,46 +118,68 @@ int main ()
 	while (!WindowShouldClose())
 	{
         frameTime = GetFrameTime();
-        if (IsKeyDown(KEY_RIGHT)) {
-            player.hitbox.x += frameTime * player.speed;
+
+        if (!player.isDead) {
+            if (IsKeyDown(KEY_RIGHT)) {
+                player.hitbox.x += frameTime * player.speed;
+            }
+            if (IsKeyDown(KEY_LEFT)) {
+                player.hitbox.x -= frameTime * player.speed;
+            }
         }
-        if (IsKeyDown(KEY_LEFT)) {
-            player.hitbox.x -= frameTime * player.speed;
-        }
-        /*if (IsKeyDown(KEY_UP)) {
-            player.hitbox.y -= frameTime * player.speed;
-        }
-        if (IsKeyDown(KEY_DOWN)) {
-            player.hitbox.y += frameTime * player.speed;
-        }*/
+
         player.damageTimer += frameTime;
-        obstacleTimer += frameTime;
-        if (obstacleTimer >=5) {
-            obstacles.push_back(spawnObstacle());
-            obstacleTimer = 0;
+        objectTimer += frameTime;
+        if (objectTimer >= 1) {
+            float objectProbability = (float)rand() / (float)RAND_MAX;
+            std::cout << objectProbability << "\n";
+            WorldObject *newObject;
+            if (objectProbability <= 0.3) {
+                newObject = spawnFuel();
+            } else {
+                newObject = spawnObstacle();
+            }
+            objects.push_back(newObject);
+            objectTimer = 0;
         }
         if (!player.isDead && player.health <= 0) {
             std::cout << "player dead\n";
             player.isDead = true;
         } else if (!player.isDead && player.damageTimer >= player.damageCooldown) {
-            Obstacle* o = checkObstacleCollisions(player, obstacles);
+            WorldObject* o = checkObjectCollisions(player, objects);
             if (o != NULL) {
-                player.health -= o->damage;
-                player.damageTimer = 0.0f;
-                std::cout << "health: " << player.health << "\n";
+                o->action(player);
             }
         }
 
+        //for (auto& o : obstacles) {
+        for (int i = 0; i < objects.size(); i++) {
+            objects[i]->hitbox.y += frameTime * flyingSpeed;
+            if (objects[i]->hitbox.y > SCREEN_HEIGHT + 200) {
+                delete objects[i];
+                objects.erase(objects.begin() + i);
+            }
+        }
 
+        if (player.fuel >= 0) {
+            player.fuel -= frameTime * player.fuelConsumption;
+        } else {
+            player.health = 0;
+        }
 
 		// drawing
 		BeginDrawing();
             ClearBackground(BLACK);
+            DrawText(std::to_string(player.fuel).c_str(), 10, 10, 30, WHITE);
+            DrawText(std::to_string(player.health).c_str(), 10, 50, 30, WHITE);
+            if (player.isDead) {
+                DrawText(deadText.c_str(), (SCREEN_WIDTH/2) - (deadTextWidth/2), (SCREEN_HEIGHT/2)-35, 70, RED);
+            }
             //std::cout << player.hitbox.x << " " << player.hitbox.y << "\n";
             DrawRectangleRec(player.hitbox, RAYWHITE);
             //DrawRectangleRec(o.hitbox, RED);
-            for (auto& o : obstacles) {
-                DrawTexturePro(o->texture, Rectangle{0,0,64,64}, o->hitbox, Vector2{0,0}, 0.0f, WHITE);
+            for (auto& o : objects) {
+                DrawTexturePro(o->texture, o->textureSize, o->hitbox, Vector2{0,0}, 0.0f, WHITE);
             }
 		EndDrawing();
 	}
